65d390c44163711d676b6cc35e7ba64240283e2c
code-azeemahmad <azeemahmadd9@gmail.com>
2025-12-17 22:28:28 +0500

    Looping Through Arrays

---
+++ Frontend/JS/Arrays/loop_through_arrays.js
@@ -0,0 +1,31 @@
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+// for (let i = 0; i <= arr.length; i++) {
+//     const element = arr[i];
+//     console.log(i);
+// }
+
+
+// arr.forEach((value, index, arr) => {
+//     console.log(value, index, arr);
+// });
+
+
+let obj  = {
+    a: 1,
+    b: 2,
+    c: 3
+}
+
+// for (const key in obj) {
+//     if (Object.prototype.hasOwnProperty.call(obj, key)) {
+//         const element = obj[key];
+//         console.log(key, element);
+        
+//     }
+// }
+
+
+// for (const iterator of arr) {
+//     console.log(iterator);
+// }
